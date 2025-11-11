6ea090e473525eeae87ac4e1af3b930f93ded87b
GustavoSilva2005 <gustavopereirasilva376@gmail.com>
2025-11-10 21:55:56 -0300

    Update Conversor_temperatura.py

--- Projeto Gustavo/Projeto_iniciante/Conversor_temperatura.py
+++ Projeto Gustavo/Projeto_iniciante/Conversor_temperatura.py
@@ -1,12 +1,18 @@
 def C_para_f(c):
     return c * 9.0 / 5.0 + 32.0
+
 def f_para_C(f):
-    return f 
+    return (f - 32) / 1.8
+
 def C_para_K(c):
-    return
+    return c + 273.15
+
 def K_para_C(k):
-    return
+    return k - 273.15
+
 def F_para_K(f):
-    return
+    return (f - 32) * 5/9 - 273
+
 def K_para_f(k):
-    return
+    return (k - 273) * 1.8 + 32
+
